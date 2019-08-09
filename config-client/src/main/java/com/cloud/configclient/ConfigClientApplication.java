63f981183c5854e7178a024f26481b44ff2330df
lhf <1490030544@qq.com>
2019-08-09 15:40:58 +0800

    呵呵

--- config-client/src/main/java/com/cloud/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/cloud/configclient/ConfigClientApplication.java
@@ -1,24 +1,21 @@
 package com.cloud.configclient;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
 public class ConfigClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
     }
-    @Value("${config.username}")
-    private String username;
-    @RequestMapping("/getuser")
-    public String getUser(){
-        return username;
-    }
-
 }
--- config-server/src/main/java/com/cloud/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/cloud/configserver/ConfigServerApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableConfigServer
 @EnableEurekaClient
-public class ConfigServerApplication {
+public class ConfigServerApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
